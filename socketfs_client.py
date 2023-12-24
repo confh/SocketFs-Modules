d241d7f997474599d0e3b832a27332087c75dac4
Confis <onexite1@gmail.com>
2023-12-24 18:48:32 +0200

    update

--- socketfs_client.py
+++ socketfs_client.py
@@ -6,24 +6,28 @@ class SocketFs:
     def _on_message(self, ws, message):
         data = json.loads(message)
         for eventHandler in self._event_handlers:
-            if eventHandler.name == data.name:
-                eventHandler.callback(data.data[0])
+            if eventHandler["name"] == data["name"]:
+                eventHandler["callback"](data["data"])
     
     def __init__(self, url: str, on_open: any = None, on_error: any = None, on_close: any = None) -> None:
         self.url = url
         self._event_handlers = []
-        self.on_open = on_open
-        self.on_error = on_error
-        self.on_close = on_close
         self.ws = websocket.WebSocketApp(url, on_message=self._on_message, on_open = on_open, on_error = on_error, on_close = on_close)
         
-    def on(self, name: str, callback: any):
-        self._event_handlers.append({"name": name, "callback": callback})
+    def send(self, eventName: str, data: any = None):
+        """ Send data to the socket server """
+        self.ws.send(json.dumps({"eventName": eventName, "data": data}))
+        
+    @property
+    def on(self):
+        """ Register event """
+        def wrapper(func):
+            self._event_handlers.append({"name": func.__name__, "callback": func})
+            return func
+        return wrapper
         
     def run(self, reconnect=5):
-        self.ws.run_forever(reconnect=reconnect)
+        """ Run the socket client """
+        self.ws.run_forever(reconnect=reconnect, dispatcher=rel)
         rel.signal(2, rel.abort)
         rel.dispatch()
-        
-    def enableTrace(enable: bool):
-        websocket.enableTrace(enable)
--- socketfs_server.ts
+++ socketfs_server.ts
@@ -1,5 +1,4 @@
 import WebSocket from 'ws';
-import events from "events"
 
 export interface SocketInterface {
     on: Function,
@@ -7,24 +6,92 @@ export interface SocketInterface {
     id: string
 }
 
-export class SocketFs extends events {
-    private ws: WebSocket.Server
+interface eventCallback {
+    eventName: string,
+    callback: Function
+}
 
+interface socketData {
+    eventName: string,
+    data?: any
+}
+
+export class SocketFs {
+    private ws: WebSocket.Server
+    private eventHandlers: eventCallback[] = []
+    private socketEventHandlers: eventCallback[] = []
+    public clients: Set<WebSocket>
+
+    /**
+     * Run the socket server
+     * @param port Port to connect to
+     */
     connect(port = 8080) {
         this.ws = new WebSocket.Server({ port });
-        this.ws.on("connection", (ws: any) => {
+        this.ws.on("connection", (ws: SocketInterface) => {
             ws.id = this.getUniqueID()
-            this.emit("connection", ws)
+            this.clients = this.ws.clients
+            ws.on("message", (message: string) => {
+                const data: socketData = JSON.parse(message)
+                for (let i = 0; i < this.socketEventHandlers.length; i++) {
+                    const event = this.socketEventHandlers[i]
+                    if (event.eventName === data.eventName) {
+                        event.callback(data.data)
+                        break
+                    }
+                }
+            })
+            this.emit("connected", ws)
+        })
+        this.ws.on("close", (ws: SocketInterface) => {
+            this.clients = this.ws.clients
+            this.emit("disconnected", ws)
         })
     }
 
-    broadcast(eventName: string, ...args: any[]) {
-        this.ws.clients.forEach(client => {
-            client.send(JSON.stringify({ name: eventName, data: args }))
+    on(eventName: string, callback: Function) {
+        this.eventHandlers.push({ eventName, callback })
+    }
+
+    onSocketEvent(ws: SocketInterface, eventName: string, callback: Function) {
+        this.socketEventHandlers.push({ eventName, callback })
+    }
+
+    emit(eventName: string, ...args: any[]) {
+        for (let i = 0; i < this.eventHandlers.length; i++) {
+            const event = this.eventHandlers[i]
+            if (event.eventName === eventName) {
+                event.callback(...args)
+                break
+            }
+        }
+    }
+
+    /**
+     * Send data to all clients.
+     * @param eventName Name of the event
+     * @param args Arguments to send data with
+     */
+    broadcast(eventName: string, data: any) {
+        this.ws.clients.forEach((client: any) => {
+            if (client !== this.ws && client.readyState === WebSocket.OPEN) {
+                client.send(this.stringifySendData(eventName, data))
+            }
         })
     }
 
-    getUniqueID = function () {
+    /**
+     * Converts send data into stringified json
+     * @param eventName Name of the event.
+     * @param args Arguments to send with data
+     * @returns Stringified JSON
+     */
+
+    stringifySendData(eventName: string, data: any): string {
+        return JSON.stringify({ name: eventName, data })
+    }
+
+    private getUniqueID = function () {
         function s4() {
             return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
         }
